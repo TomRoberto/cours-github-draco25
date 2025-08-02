06a5567c8f50b95f5179d8d17ff7b93d910a09b4
Tom ROBERTO <tom.roberto31@gmail.com>
2025-08-02 16:38:30 +0200

    chenged header color and text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,9 @@
 const Header = () => {
-  return <header style={{ backgroundColor: "green" }}>Header</header>;
+  return (
+    <header style={{ backgroundColor: "green", color: "red" }}>
+      Header de Tom
+    </header>
+  );
 };
 
 export default Header;
